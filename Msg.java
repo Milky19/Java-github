1a08965eaa82e6b96865e4431659846ac5500949
Milky19 <97377003+Milky19@users.noreply.github.com>
2026-05-04 19:34:13 +0530

    Update Msg.java

--- Msg.java
+++ Msg.java
@@ -1,13 +1,13 @@
 public class Msg {
 	public static void main(String[] args){
 		int eno=1001;
-		String ename="KRISHNA"
-		String eaddr="Hyd"
+		String ename="KRISHNA";
+		String eaddr="Hyd";
 		System.out.println("Display emp information");
 		System.out.println("Emp eno is:"+eno);
 		System.out.println("emp name is:"+ename);
 		System.out.println("emp address is:"+eaddr);
-		System.out.println("Thanks for using My app:")
+		System.out.println("Thanks for using My app:");
 	}
 }
 
