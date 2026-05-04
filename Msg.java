5c02931120218aa77874751dc433c5f4124d48a7
root <root@ip-172-31-45-131.ec2.internal>
2026-05-04 13:52:31 +0000

    java-code

---
+++ Msg.java
@@ -0,0 +1,13 @@
+public class Msg {
+	public static void main(String[] args){
+		int eno=1001;
+		String ename="KRISHNA"
+		String eaddr="Hyd"
+		System.out.println("Display emp information");
+		System.out.println("Emp eno is:"+eno);
+		System.out.println("emp name is:"+ename);
+		System.out.println("emp address is:"+eaddr);
+		System.out.println("Thanks for using My app:")
+	}
+}
+
